dc8256f14d2468ff0dec50335eaedb847d9e893f
Caioviniciusb <caiovinicius.barros@hotmail.com>
2021-04-06 10:41:56 -0300

    Atualizando o programa

--- Prog_estruturada/comandos_de_selecao.c
+++ Prog_estruturada/comandos_de_selecao.c
@@ -11,7 +11,9 @@ int main(void){
 
     if (hora > 12) {
         hora -= 12;
-
+    }
+    else if (hora == 00) {
+        hora += 12;
     }
     printf("\nA hora em formato de 12 horas é %.2d:%.2d\n", hora, min);
 
